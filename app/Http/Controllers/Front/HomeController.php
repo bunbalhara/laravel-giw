b32ec72c2573a8bb485c69d68643a502dfb4cde7
bunbalhara <nanjingzhe425@gmail.com>
2021-02-04 19:26:10 +0800

    commit

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -43,11 +43,9 @@ class HomeController extends Controller
         if($validator->passes()){
             try {
 
-                $headers = "MIME-Version: 1.0" . "\r\n";
-                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-                $headers .= 'From: info@giw.com.au' . "\r\n" .
+                $headers = 'From: info@giw.com.au' . "\r\n" .
                     'Reply-To: info@giw.com.au' . "\r\n" .
+                    "Content-type: text/html;charset=UTF-8" . "\r\n".
                     'X-Mailer: PHP/' . phpversion();
 
                 $email = $request->email;
